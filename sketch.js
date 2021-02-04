6239eda3fbbb5a33a8eb39907b89e9f91e9195b0
WildTrooper <76808816+WildTrooper@users.noreply.github.com>
2021-02-04 15:04:14 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,72 +1,49 @@
 const Engine = Matter.Engine;
-const World= Matter.World;
+const World = Matter.World;
 const Bodies = Matter.Bodies;
-const Constraint = Matter.Constraint;
 
-var engine, world;
-var box1, pig1;
-var backgroundImg,platform;
-var constrainedLog;
+var bgImage,bg;
+var fairy,fairyImage;
+var starImage,star;
 
-function preload() {
-    backgroundImg = loadImage("sprites/bg.png");
-}
-
-function setup(){
-    var canvas = createCanvas(1200,400);
-    engine = Engine.create();
-    world = engine.world;
-
-
-    ground = new Ground(600,height,1200,20);
-    platform = new Ground(150, 305, 300, 170);
-
-    box1 = new Box(700,320,70,70);
-    box2 = new Box(920,320,70,70);
-    pig1 = new Pig(810, 350);
-    log1 = new Log(810,260,300, PI/2);
-
-    box3 = new Box(700,240,70,70);
-    box4 = new Box(920,240,70,70);
-    pig3 = new Pig(810, 220);
-
-    log3 =  new Log(810,180,300, PI/2);
-
-    box5 = new Box(810,160,70,70);
-    log4 = new Log(760,120,150, PI/7);
-    log5 = new Log(870,120,150, -PI/7);
-
-    bird = new Bird(100,100);
-    constrainedLog = new Log(230,180,80,PI/2);
-    chain = new Chain(bird.body,constrainedLog.body)
+function preload(){
+bgImage=loadImage("starnight.png")
+fairyImage=loadAnimation("fairy1.png","fairy2.png")
+starImage=loadImage("star.png")
 }
 
 
+function setup() {
+  createCanvas(800, 750);
+  background("white")
 
-function draw(){
-    background(backgroundImg);
-    Engine.update(engine);
-    console.log(box2.body.position.x);
-    console.log(box2.body.position.y);
-    console.log(box2.body.angle);
-    box1.display();
-    box2.display();
-    ground.display();
-    pig1.display();
-    log1.display();
+  bg = createSprite(400,370.800,800)
+  bg.addImage(bgImage)
 
-    box3.display();
-    box4.display();
-    pig3.display();
-    log3.display();
+  fairy= createSprite (100,600)
+  fairy.addAnimation("flyingFairy",fairyImage);
+  fairy.scale = 0.3
 
-    box5.display();
-    log4.display();
-    log5.display();
+  star = createSprite(700,100,20,20)
+  star.addImage(starImage)
+  star.scale = 0.4
+  }
+  
 
-    bird.display();
-    platform.display();
-    chain.display();
-    constrainedLog.display();
 
+
+function draw() {
+  if(keyDown("left_arrow")){
+  fairy.x -= 5
+  }
+  if(keyDown("right_arrow")){
+  fairy.x += 5
+ }
+ if(keyDown("down_arrow")){
+ star.velocityY = 4
+}
+if (star.position.y > 470){
+  Matter.body.setStatic(star,true)
+}
+ drawSprites();
 }
